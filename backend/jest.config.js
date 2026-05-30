1738e0c0c818ef1d47a0c7dd409270992fd50eaa
Tim <joshuatimothy976@gmail.com>
2026-05-30 14:14:45 +0100

    chore: add Jest configuration with 80% coverage threshold for backend

---
+++ backend/jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  displayName: 'backend',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    '**/*.(t|j)s',
+    '!**/*.spec.ts',
+    '!**/node_modules/**',
+  ],
+  coverageDirectory: '../coverage',
+  coverageReporters: ['text', 'lcov', 'json', 'html'],
+  testEnvironment: 'node',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
